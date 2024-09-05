e0c40149c336ee202b9faa7b01005655c307c265
morenicogs <nicolas@grootjans.co>
2024-09-06 00:16:23 +0200

    fixed

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,25 @@
-const staticCacheName = "site-static-v6"
-const dynamicCache = "site-dynamic-v6"
+const staticCacheName = "site-static-v7"
+const dynamicCache = "site-dynamic-v7"
 const assets = [
 	"/",
-	"./index.html",
-	"./overview.html",
-	"./scripts/libraries/luxon.min.js",
-	"./scripts/app.js",
-	"./scripts/buttons.js",
-	"./scripts/loadApp.js",
-	"./scripts/loadOverview.js",
-	"./scripts/localStorage.js",
-	"./scripts/overview.js",
-	"./scripts/project.js",
-	"./scripts/recording.js",
-	"./scripts/render.js",
-	"./scripts/scene.js",
-	"./scripts/take.js",
-	"./style.css",
+	"https://nicogs.github.io/slayed/index.html",
+	"https://nicogs.github.io/slayed/overview.html",
+	"https://nicogs.github.io/slayed/scripts/libraries/luxon.min.js",
+	"https://nicogs.github.io/slayed/scripts/app.js",
+	"https://nicogs.github.io/slayed/scripts/buttons.js",
+	"https://nicogs.github.io/slayed/scripts/loadApp.js",
+	"https://nicogs.github.io/slayed/scripts/loadOverview.js",
+	"https://nicogs.github.io/slayed/scripts/localStorage.js",
+	"https://nicogs.github.io/slayed/scripts/overview.js",
+	"https://nicogs.github.io/slayed/scripts/project.js",
+	"https://nicogs.github.io/slayed/scripts/recording.js",
+	"https://nicogs.github.io/slayed/scripts/render.js",
+	"https://nicogs.github.io/slayed/scripts/scene.js",
+	"https://nicogs.github.io/slayed/scripts/take.js",
+	"https://nicogs.github.io/slayed/style.css",
 	"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
 	"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js",
-	"./img/icons/icon-192x192.png"
+	"https://nicogs.github.io/slayed/img/icons/icon-192x192.png"
 ]
 
 // Install service worker
